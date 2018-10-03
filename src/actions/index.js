0507d2f9612c9045380723ff86befc0affae5186
Ethan Ng <ethanng329@gmail.com>
2018-10-03 12:03:45 +0100

    added comment

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,19 @@
-export function fetchUsers(){
-  return function (dispatch) {
+// fetch users list via API from jsonplaceholder
+export function fetchUsers() {
+  return function(dispatch) {
     fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then(response => response.json())
-    .then(data => {
-      dispatch(setUsersToStore(data));
-    })
-    .catch(error => console.log(error))
-
-  }
+      .then(response => response.json())
+      .then(data => {
+        dispatch(setUsersToStore(data));
+      })
+      .catch(error => console.log(error));
+  };
 }
 
+//action to call reducer to save data to store
 export function setUsersToStore(users) {
   return {
-    type:'USERS_LIST',
+    type: 'USERS_LIST',
     users
-  }
+  };
 }
